834548d17ccb851a12bda8a294b48ac2aa0c30f1
Geeteshgit <143749482+Geeteshgit@users.noreply.github.com>
2024-12-19 15:55:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      'laptop' : '1180px',
+      'tablet' : '780px',
+      'phone' : '530px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
